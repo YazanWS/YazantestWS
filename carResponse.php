ea547035f1dac61fc6efb8e178d8f8f2c0b876a3
YazanWS <yazan.sobh@dawsoncollege.qc.ca>
2025-09-24 18:49:32 -0400

    df

--- carResponse.php
+++ carResponse.php
@@ -30,6 +30,19 @@
 <body>
     <div class="response">
         <h1>Form Submission Results</h1>
+        <?php
+            $carNames = [
+                "celica" => "1986 Toyota Celica",
+                "datsun" => "1983 Datsun 280ZX Turbo",
+                "mercedes" => "1995 Mercedes-Benz W140",
+                "prelude" => "1995 Honda Prelude"
+            ];
+            if (isset($_POST['favoriteCar']) && array_key_exists($_POST['favoriteCar'], $carNames)) {
+                echo "<p>You selected: <strong>" . $carNames[$_POST['favoriteCar']] . "</strong></p>";
+            } else {
+                echo "<p>No car was selected.</p>";
+            }
+        ?>
         <p><strong>GET:</strong> <?= var_dump($_GET) ?></p>
         <p><strong>POST:</strong> <?= var_dump($_POST) ?></p>
     </div>
