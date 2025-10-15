f554faddde14eb543fde14fb87154e6675e5d553
YazanWS <yazan.sobh@dawsoncollege.qc.ca>
2025-10-15 10:51:06 -0400

    fg

--- submit_favorite.php
+++ submit_favorite.php
@@ -5,10 +5,9 @@
     <title>Form response</title>
 </head>
 <?php
-    // Retrieve submitted information
+    $name = htmlspecialchars($_POST["name"]);
     $favorite_car = htmlspecialchars($_POST["favoriteCar"]);
 
-    // Database connection settings
     $server = "localhost";
     $username = "yazan";
     $password = "Mango3990";
@@ -16,16 +15,14 @@
 
     $conn = mysqli_connect($server, $username, $password, $database);
 
-    // Check for successful connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Insert into favoriteauto table
-    $sql = "INSERT INTO favoriteauto (car) VALUES ('$favorite_car');";
+    $sql = "INSERT INTO favoriteauto (name, car) VALUES ('$name', '$favorite_car');";
     $result = mysqli_query($conn, $sql);
 ?>
 <body>
-    <p>My favorite car is <?= $favorite_car ?></p>
+    <p>Thank you, <?= $name ?>! Your favorite car is <?= $favorite_car ?>.</p>
 </body>
 </html>
