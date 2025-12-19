1494823779940a45b807d15b7d5be100ea5ff31c
YazanWS <yazan.sobh@dawsoncollege.qc.ca>
2025-12-19 11:26:04 -0500

    dg

--- g00gle.php
+++ g00gle.php
@@ -1,39 +1,42 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Google</title>
-    <body>
-        <h1>Google Search</h1>
-        <form method="POST">
-        <label for="search">Search Google or Type URL</label>
-        <input type="text" id="search" name="search" required>
-        <input type="submit" value="Search">
-    </form>
-      <script>
-        <?php
-        $ip = $_SERVER["REMOTE_ADDR"];
+    <head>
+        <title>Google</title>
+    </head>
+        <body>
+            <h1>Google Search</h1>
+            <form method="POST">
+            <label for="search">Search Google or Type URL</label>
+            <input type="text" id="search" name="search" required>
+            <input type="submit" value="search">
+            </form>  
+        </body>
+
+        <script>
+            <?php
             
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $search = htmlspecialchars($_POST["search"]);
-        }
-        $server = "localhost";
-        $username = "yazan";
-        $password = "Mango3990";
-        $database = "g00gle";
+            $ip = $_SERVER["REMOTE_ADDR"];
+                
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $search = htmlspecialchars($_POST["search"]);
+            }
+            $server = "localhost";
+            $username = "yazan";
+            $password = "Mango3990";
+            $database = "g00gle";
 
-        $conn = mysqli_connect($server, $username, $password, $database);
-        $result = mysqli_query($conn, $sql);
+            $conn = mysqli_connect($server, $username, $password, $database);
+            $result = mysqli_query($conn, $sql);
 
-        if($result){
-            echo "You have searched for $search from IP address $ip";
-        } else {
-            echo "Error: " . mysqli_error($conn);
-        }
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-         $sql = "INSERT INTO searchinfo (IP, search) VALUES ('$ip', '$search');"
-        ?>
-        </script> 
-    </body>
+            
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            $sql = "INSERT INTO searchinfo (IP, search) VALUES ('$ip', '$search');"
+             
+            echo "You have searched for $search from IP address $ip";    
+            ?>
+            </script> 
+        echo "You have searched for $search from IP address $ip";    
+    
 </html>
